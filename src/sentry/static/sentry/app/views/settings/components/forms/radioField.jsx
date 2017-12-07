250c30f58f898b5fb360a3e4aa4d2308228630a5
chrissy <fivefourths@gmail.com>
2017-12-07 13:49:09 -0800

    add in basic styles for radio button components

--- src/sentry/static/sentry/app/views/settings/components/forms/radioField.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/radioField.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'react-emotion';
+import PropTypes from 'prop-types';
+
 import InputField from './inputField';
 
-export default class RadioField extends InputField {
+class RadioField extends InputField {
+  static propTypes = {
+    id: PropTypes.integer,
+    value: PropTypes.string,
+  };
+
   onChange = (value, onChange, onBlur, e) => {
     onChange(value, e);
     onBlur(value, e);
@@ -20,14 +28,15 @@ export default class RadioField extends InputField {
             {(props.choices() || []).map(choice => {
               const {id, name} = choice;
               return (
-                <div
+                <RadioLineItem
                   key={id}
-                  value={id}
                   onClick={this.onChange.bind(this, id, onChange, onBlur)}
-                  style={{fontWeight: this.isSelected({value, id}) ? 'bold' : 'inherit'}}
                 >
-                  {name}
-                </div>
+                  <RadioLineButton>
+                    {this.isSelected({value, id}) ? <RadioLineButtonFill /> : ''}
+                  </RadioLineButton>
+                  <RadioLineText>{name}</RadioLineText>
+                </RadioLineItem>
               );
             })}
           </div>
@@ -36,3 +45,38 @@ export default class RadioField extends InputField {
     );
   }
 }
+
+const RadioLineItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5em;
+  cursor: pointer;
+`;
+
+const RadioLineButton = styled.div`
+  width: 1.5em;
+  height: 1.5em;
+  position: relative;
+  border-radius: 50%;
+  border: 1px solid ${p => p.theme.borderLight};
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.04);
+`;
+
+const RadioLineButtonFill = styled.div`
+  width: 54%;
+  height: 54%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  background-color: ${p => p.theme.green};
+`;
+
+const RadioLineText = styled.div`
+  margin-left: 0.5em;
+  font-size: 0.875em;
+  font-weight: bold;
+`;
+
+export default RadioField;
